9b3e7111e881643ea066ec601941995bbcbbd851
palerme <joaoguilhermefagundes@gmail.com>
2022-12-01 16:22:06 -0300

    crud php laravel trabalho html css javascript

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    protected $model;
+    public function __construct(Cliente $cliente)
+    {
+
+        $this->model = $cliente;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        return response($this->model->all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $this->model->create($request->all());
+            return response('Criado com sucesso');
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $cliente = $this->model->find($id);
+
+        if (!$cliente) {
+            return response('Cliente não encontrado!');
+        }
+        return response($cliente);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $cliente = $this->model->find($id);
+        if(!$cliente)
+        {
+            return response('Cliente não encontrado');
+        }
+
+        try {
+            $dados = $request->all();
+            $cliente->fill($dados)->save();
+            return response('Cliente atualizado');
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $cliente = $this->model->find($id);
+        if(!$cliente) 
+        {
+            return response('Cliente não encontrado');
+        }
+        try {
+            $cliente->delete();
+            return response('Cliente excluido');
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ClienteController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/index', [ClienteController::class, 'index'])->name('api.index');
+Route::post('/store', [ClienteController::class, 'store'])->name('api.store');
+Route::get('/edit/{id}', [ClienteController::class, 'edit'])->name('api.edit');
+Route::delete('/delete/{id}', [ClienteController::class, 'destroy'])->name('api.destroy');
+Route::put('/update/{id}',[ClienteController::class, 'update'])->name('api.update');
